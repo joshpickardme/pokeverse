88f7f9fd4185a319d6c6ed528907fc61b3839873
Josh <hello@joshpickard.me>
2024-02-04 18:18:40 +0000

    card component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,18 +11,38 @@ import { useEffect, useState } from "react";
 export default function Home() {
 
 
-  const [pokemon, setPokemon] = useState([])
-  
+  const [pokemonState, setPokemonState] = useState([])
+  const array = []
+
+  async function fetchPokemon() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    const pokemon = await response.json()
+    for (let i = 0; i < pokemon.results.length; i++) {
+      const pokeResponse = await fetch(pokemon.results[i].url)
+      const pokeJson = await pokeResponse.json()
+      pokeJson.squad = false
+      await array.push(pokeJson)
+
+    }
+    console.log(array)
+    setPokemonState(array)
+  }
+
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-      .then((response) => response.json())
-      .then((data) => setPokemon(data.results))
-      .catch((error) => console.error(error))
+    fetchPokemon()
   }, [])
 
   return (
-    <main className="flex flex-wrap justify-center min-h-screen gap-3 p-4">
-      {pokemon.map(pokemon => <Card key={pokemon.name} name={pokemon.name}></Card>)}
+    <main className="flex justify-center items-center flex-col gap-3 p-4">
+      <h1 className="text-white text-4xl">My Squad (0)</h1>
+      <div className="flex flex-row flex-wrap justify-center gap-3 p-4">
+        {pokemonState.filter(pokemon => pokemon.squad).map(filteredPokemon => (<Card pokemonState={pokemonState} pokemon={filteredPokemon} setPokemonState={setPokemonState} key={filteredPokemon.id}></Card>))}
+      </div>
+      <h1 className="text-white text-4xl">All Pokemon</h1>
+      <div className="flex flex-row flex-wrap justify-center min-h-screen gap-3 p-4">
+        {pokemonState.map(pokemon => <Card pokemonState={pokemonState} pokemon={pokemon} setPokemonState={setPokemonState} key={pokemon.id}></Card>)}
+      </div>
     </main>
+
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,29 @@
-export default function Card({name, level, image, skill}) {
+"use client"
+export default function Card({pokemonState, pokemon, setPokemonState}) {
+
+    function updateSquad() {
+        pokemon.squad = !pokemon.squad
+        console.log(pokemon)
+        setPokemonState((prevPokemonState) => [...prevPokemonState, pokemon]);
+        console.log(pokemonState)
+    }
+
     return (
-        <div className="card w-96 h-96 bg-base-100 shadow-xl">
-            <figure><img src={"https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                {name}
-                <div className="badge badge-secondary">Level {level}</div>
-                </h2>
-                <p>{skill}</p>
-                <div className="card-actions justify-end">
-                </div>
+        <div className="card w-96 bg-base-100 shadow-xl image-full">
+        <img className=" opacity-25" src={pokemon.sprites.other.home.front_default} alt="pokemon" />
+        <div className="card-body justify-between">
+            <div className="flex flex-row justify-between w-full">
+                <h2 className="card-title text-white">{pokemon.name}</h2>
             </div>
+            <div className="flex flex-col gap-2">
+                <p>Experience: {pokemon.base_experience}</p>
+                <p>Height: {pokemon.height}</p>
+                <p>Squad: {String(pokemon.squad)}</p>
+            </div>
+            <div className="flex flex-row w-full justify-start gap-4 ">
+                <button onClick={updateSquad} className="w-32 cursor-pointer p-4 rounded-md bg-gray-600 text-white font-bold hover:scale-110 transition-all">Add</button>
+            </div>
+        </div>
         </div>
     )
 }
